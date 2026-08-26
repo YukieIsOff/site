0b5121a72bf2e8df27e7067cd4e906bbd231149e
YukieIsOff <nana.c.none@gmail.com>
2026-08-25 22:03:12 -0300

    Adicionar site

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const elements = document.querySelectorAll(".reveal");
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+        }
+    });
+});
+elements.forEach((element) => {
+    observer.observe(element);
+});
